b464735abf35db59c7df72228e0454ba9188a8b2
oleksiyp <oleksiy.pylypenko@gmail.com>
2011-11-04 17:41:00 +0200

    Added crawler script

--- src/main/resources/org/loadtest/Classes.groovy
+++ src/main/resources/org/loadtest/Classes.groovy
@@ -119,3 +119,35 @@ class HTTP {
 }
 
 
+public class Globals {
+    private Map values = new HashMap();
+
+    public synchronized void put(String name, Object value) {
+        values.put(name, value);
+    }
+
+    public synchronized Object get(String name) {
+        return values.get(name);
+    }
+
+    public synchronized List list(String name, String []defaultValues) {
+        List list = (List) values.get(name);
+        if (list == null) {
+            list = Collections.synchronizedList(new ArrayList(Arrays.asList(defaultValues)));
+            values.put(name, list);
+        }
+        return list;
+    }
+
+    public synchronized long increment(String name) {
+        Number num = (Number)values.get(name);
+        if (num == null) {
+            num = 0L;
+        }
+        long res = num.longValue();
+        values.put(name, res + 1);
+        return res;
+    }
+}
+
+
