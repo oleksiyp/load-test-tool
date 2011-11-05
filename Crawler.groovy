e2aef7a44c275b7bcf0c1ad556dc3761dca7dfbe
oleksiyp <oleksiy.pylypenko@gmail.com>
2011-11-05 14:48:06 +0200

    Refactoring. Bug with global exists

--- Crawler.groovy
+++ Crawler.groovy
@@ -1,5 +1,5 @@
 // example of simple crawler
-// run: java -jar loadtest.jar -n 3 -d 1000 -ie 'GLOBALS.list("urls","http://google.com")' Crawler.groovy
+// run: java -jar loadtest.jar -n 3 -d 1000 -ie 'GLOBALS.instanceList("urls","http://google.com")' Crawler.groovy
 import java.util.regex.Pattern;
 
 try {
